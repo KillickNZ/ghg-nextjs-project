d146049d059492a8cb6e77a200dfe21f16eee2a1
Seb <seb@southernsoftware.nz>
2022-08-27 14:20:37 +1200

    Created typeform component and rendered on index page

---
+++ components/Typeform.tsx
@@ -0,0 +1,10 @@
+import { Widget } from '@typeform/embed-react'
+import React from 'react'
+
+type Props = {}
+
+export default function typeform({}: Props) {
+  return (
+   <Widget id="eEZaIxfr" style={{ width: '50%' }} className="my-form" />
+  )
+}
