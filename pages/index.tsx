81f3f2813edeac082ead76aaa86c6b655401c64a
KillickNZ <sebastian.g.chapman@gmail.com>
2022-08-27 19:36:59 +1200

    removed package-lock and capitalized import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Typeform from '../components/typeform'
+import Typeform from '../components/Typeform'
 
 const Home: NextPage = () => {
   return (
